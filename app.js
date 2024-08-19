4dc8102386bda7f1c42fa87289ae32fd66bd7776
ThamkeRohan <thamkerohan2@gmail.com>
2024-08-19 22:39:54 +0530

    Change CORS origin

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ app.use(
     resave: false,
     cookie: {
       maxAge: ONE_DAY,
+      secure: true, // Ensure the cookie is only sent over HTTPS
+      sameSite: "none", // Allow the cookie to be sent with cross-origin requests
     },
     store: store,
   })
